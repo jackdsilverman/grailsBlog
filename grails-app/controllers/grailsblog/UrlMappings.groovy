c3913988650520c59006801c60e103af1d3e0a57
Jack D. Silverman <Jsilverman@manifestcorp.com>
2018-03-15 08:32:22 -0400

    changed initial mapping to BlogPost index

--- grails-app/controllers/grailsblog/UrlMappings.groovy
+++ grails-app/controllers/grailsblog/UrlMappings.groovy
@@ -3,13 +3,7 @@ package grailsblog
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
-
-        "/"(view:"/index")
+        "/"(controller: 'BlogPost',view: "index", action: "index")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
